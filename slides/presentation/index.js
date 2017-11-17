cff71012819f9f31cdbed7b77f4b561097881df4
Hector Garcia <hectahertz@gmail.com>
2017-11-17 15:12:31 +0100

    💅🏻 Base styling

--- slides/presentation/index.js
+++ slides/presentation/index.js
@@ -2,17 +2,7 @@
 import React from "react";
 
 // Import Spectacle Core tags
-import {
-  BlockQuote,
-  Cite,
-  Deck,
-  Heading,
-  ListItem,
-  List,
-  Quote,
-  Slide,
-  Text
-} from "spectacle";
+import { BlockQuote, Cite, Deck, Heading, Quote, Slide, Text } from "spectacle";
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
@@ -20,49 +10,74 @@ import createTheme from "spectacle/lib/themes/default";
 // Require CSS
 require("normalize.css");
 
-const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quartenary: "#CECECE"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+// Assets
+const reactLogo = require("../assets/react.svg");
+
+const theme = createTheme(
+  {
+    primary: "#0B0F13",
+    secondary: "#F6F6F6",
+    tertiary: "#41B883",
+    quartenary: "#F6F6F6",
+
+    mint: "#41B883",
+    charcoal: "#35495E",
+    auroMetalSaurus: "#727F80",
+    whiteSmoke: "#F6F6F6",
+    white: "#FFFFFF"
+  },
+  {
+    primary: { name: "Oswald", googleFont: true, styles: ["300"] },
+    secondary: { name: "Open Sans", googleFont: true, styles: ["300"] }
+  }
+);
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+      <Deck transition={["slide"]} transitionDuration={500} theme={theme}>
+        <Slide
+          bgImage={reactLogo}
+          transition={["slide"]}
+          bgPosition="0% 20%"
+          bgRepeat="no-repeat"
+        >
+          <Heading size={1} caps lineHeight={1.2} textColor="mint">
+            React Native workshop
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
+          <Heading
+            size={5}
+            margin="32px 0 0 0"
+            textColor="whiteSmoke"
+            textFont="secondary"
+          >
+            ✨ Let's rebuild Twitter ✨
+          </Heading>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+        <Slide bgImage={reactLogo} transition={["slide"]}>
+          <Heading size={6} textColor="mint" caps>
+            Typography
+          </Heading>
+          <Heading size={1} textColor="whiteSmoke">
+            Heading 1
+          </Heading>
+          <Heading size={2} textColor="whiteSmoke">
+            Heading 2
+          </Heading>
+          <Heading size={3} textColor="whiteSmoke">
+            Heading 3
+          </Heading>
+          <Heading size={4} textColor="whiteSmoke">
+            Heading 4
+          </Heading>
+          <Heading size={5} textColor="whiteSmoke">
+            Heading 5
+          </Heading>
+          <Text textColor="whiteSmoke">Standard text</Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+        <Slide bgImage={reactLogo} transition={["slide"]}>
           <BlockQuote>
-            <Quote>Example Quote</Quote>
+            <Quote textColor="whiteSmoke">Example Quote</Quote>
             <Cite>Author</Cite>
           </BlockQuote>
         </Slide>
