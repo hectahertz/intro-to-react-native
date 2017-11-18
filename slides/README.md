deaf777e150c5625c7f8905ebdabc9c0ae148855
hectahertz <hectahertz@gmail.com>
2017-11-18 15:56:28 +0100

    📝  Introduction outline

---
+++ slides/README.md
@@ -0,0 +1,9 @@
+# Slides for my Introduction to React Native Workshop
+
+Your first order of business is to open terminal and run `yarn`. (or `npm install` if you're about that life)
+
+Then, to start up the local server, run `npm start`
+
+Open a browser and hit [http://localhost:3000](http://localhost:3000), and we are ready to roll
+
+> Created with [Spectacle 🕶](https://github.com/FormidableLabs/spectacle-boilerplate)
--- slides/presentation/index.js
+++ slides/presentation/index.js
@@ -2,7 +2,15 @@
 import React from "react";
 
 // Import Spectacle Core tags
-import { BlockQuote, Cite, Deck, Heading, Quote, Slide, Text } from "spectacle";
+import {
+  BlockQuote,
+  Cite,
+  Deck,
+  Heading,
+  Quote,
+  Slide,
+  Image
+} from "spectacle";
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
@@ -11,7 +19,15 @@ import createTheme from "spectacle/lib/themes/default";
 require("normalize.css");
 
 // Assets
-const reactLogo = require("../assets/react.svg");
+const reactBg = require("../assets/react-bg.svg");
+const react = require("../assets/react.svg");
+const facebook = require("../assets/facebook.png");
+const instagram = require("../assets/instagram.png");
+const airbnb = require("../assets/airbnb.svg");
+const apple = require("../assets/apple.svg");
+const android = require("../assets/android.svg");
+const expo = require("../assets/expo.svg");
+const reactNative = require("../assets/react-native.svg");
 
 const theme = createTheme(
   {
@@ -37,7 +53,7 @@ export default class Presentation extends React.Component {
     return (
       <Deck transition={["slide"]} transitionDuration={500} theme={theme}>
         <Slide
-          bgImage={reactLogo}
+          bgImage={reactBg}
           transition={["slide"]}
           bgPosition="0% 20%"
           bgRepeat="no-repeat"
@@ -51,35 +67,183 @@ export default class Presentation extends React.Component {
             textColor="whiteSmoke"
             textFont="secondary"
           >
-            ✨ Let's rebuild Twitter ✨
+            ✨ Welcome to the fuuuture~ ✨
           </Heading>
         </Slide>
-        <Slide bgImage={reactLogo} transition={["slide"]}>
-          <Heading size={6} textColor="mint" caps>
-            Typography
+        <Slide transition={["slide"]}>
+          <Heading size={1} textColor="mint" caps lineHeight={1.2}>
+            What is this sorcery? 😲
           </Heading>
-          <Heading size={1} textColor="whiteSmoke">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="whiteSmoke">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="whiteSmoke">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="whiteSmoke">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="whiteSmoke">
-            Heading 5
-          </Heading>
-          <Text textColor="whiteSmoke">Standard text</Text>
         </Slide>
-        <Slide bgImage={reactLogo} transition={["slide"]}>
+        <Slide transition={["slide"]}>
+          <Heading
+            size={2}
+            textColor="whiteSmoke"
+            textFont="secondary"
+            lineHeight={1.2}
+          >
+            Not an “HTML5 app” or a “hybrid app” 🙅🏻
+          </Heading>
+        </Slide>
+        <Slide transition={["slide"]}>
           <BlockQuote>
-            <Quote textColor="whiteSmoke">Example Quote</Quote>
-            <Cite>Author</Cite>
+            <Quote lineHeight={1.2} textColor="whiteSmoke">
+              JavaScript–driven Native app
+            </Quote>
+            <Cite textFont="secondary">John Wargo</Cite>
           </BlockQuote>
+        </Slide>
+        <Slide transition={["slide"]}>
+          <Heading
+            size={1}
+            caps
+            textColor="mint"
+            lineHeight={2}
+          >
+            ✨ JS World ✨
+          </Heading>
+          <Heading
+            size={4}
+            textColor="whiteSmoke"
+            textFont="secondary"
+            lineHeight={1.2}
+          >
+            ⬆️ The bridge ⬇️
+          </Heading>
+          <Heading
+            size={1}
+            caps
+            textColor="mint"
+            lineHeight={2}
+          >
+            ✨ Native World ✨
+          </Heading>
+        </Slide>
+        <Slide transition={["slide"]}>
+          <Heading size={1} textColor="mint" caps lineHeight={1.2}>
+            Many great features!
+          </Heading>
+        </Slide>
+        <Slide transition={["slide"]}>
+          <Heading
+            size={2}
+            textFont="secondary"
+            textColor="whiteSmoke"
+            lineHeight={1.2}
+          >
+            React powered
+          </Heading>
+          <Image
+            src={react}
+            display="inline-block"
+            height={300}
+            width={300}
+            margin="32px 0px 0 0"
+          />
+        </Slide>
+        <Slide transition={["slide"]}>
+          <Heading
+            size={2}
+            textColor="whiteSmoke"
+            textFont="secondary"
+            lineHeight={1.2}
+          >
+            ⚡️ Node JS ⚡️
+          </Heading>
+        </Slide>
+        <Slide transition={["slide"]}>
+          <Heading
+            size={2}
+            textColor="whiteSmoke"
+            textFont="secondary"
+            lineHeight={1.2}
+          >
+            🔥 Hot reloading 🔥
+          </Heading>
+        </Slide>
+        <Slide transition={["slide"]}>
+          <Heading
+            size={2}
+            textColor="whiteSmoke"
+            textFont="secondary"
+            lineHeight={1.2}
+          >
+            Talks to native code! 😲
+          </Heading>
+          <Image
+            src={android}
+            display="inline-block"
+            height={196}
+            width={196}
+            margin="48px 64px 0 0"
+          />
+          <Image
+            src={apple}
+            display="inline-block"
+            height={196}
+            width={196}
+            margin="48px 0px 0 0"
+          />
+        </Slide>
+        <Slide transition={["slide"]}>
+          <Heading
+            size={1}
+            textColor="whiteSmoke"
+            textFont="secondary"
+            lineHeight={1.2}
+          >
+            Who's loving it?
+          </Heading>
+          <Image
+            src={instagram}
+            display="inline-block"
+            height={200}
+            width={200}
+            margin="48px 64px 0 0"
+          />
+          <Image
+            src={airbnb}
+            display="inline-block"
+            height={200}
+            width={200}
+            margin="48px 64px 0 0"
+          />
+          <Image
+            src={facebook}
+            display="inline-block"
+            height={200}
+            width={200}
+            margin="48px 0px 0 0"
+          />
+        </Slide>
+        <Slide transition={["slide"]}>
+          <Heading size={1} textColor="mint" caps lineHeight={1.2}>
+            Are you ready? 😍
+          </Heading>
+        </Slide>
+        <Slide transition={["slide"]}>
+          <Heading size={1} textColor="mint" caps lineHeight={1.2}>
+            Two ways to start
+          </Heading>
+          <Image
+            src={expo}
+            display="inline-block"
+            height={300}
+            width={300}
+            margin="48px 64px 0 0"
+          />
+          <Image
+            src={reactNative}
+            display="inline-block"
+            height={300}
+            width={300}
+            margin="48px 0px 0 0"
+          />
+        </Slide>
+        <Slide transition={["slide"]}>
+          <Heading size={1} textColor="mint" caps lineHeight={1.2}>
+            Let's go! 🚀
+          </Heading>
         </Slide>
       </Deck>
     );
