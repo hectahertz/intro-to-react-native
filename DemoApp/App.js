2b51f50efd10c6c26e9e6b2b4269b69d3b918590
hectahertz <hectahertz@gmail.com>
2017-11-18 17:26:33 +0100

    🔑 Add username and password fields, with icons

--- DemoApp/App.js
+++ DemoApp/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 
 export default class App extends React.Component {
-  state = { text: 'Initial state' };
+  state = { user: '', password: '' };
 
   render() {
     return (
       <View style={styles.container}>
+        <Entypo name="email" size={32} color="black" />
         <TextInput
           style={{ height: 40 }}
-          placeholder="Type here!"
-          onChangeText={text => this.setState({ text })}
+          placeholder="Enter your username"
+          onChangeText={text => this.setState({ user: text })}
         />
-        <Text style={{ padding: 10, fontSize: 42 }}>{this.state.text}</Text>
+        <Entypo name="key" size={32} color="black" />
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter your passsword"
+          onChangeText={text => this.setState({ password: text })}
+        />
+        <Text style={{ padding: 10, fontSize: 16 }}>
+          {JSON.stringify(this.state, null, 2)}
+        </Text>
       </View>
     );
   }
