432f34cac9760e5df9c56689002b5d04d6f69915
hectahertz <hectahertz@gmail.com>
2017-11-18 22:53:04 +0100

    👀 Watch users and tweets from Firebase

--- DemoApp/js/screens/Tweets.js
+++ DemoApp/js/screens/Tweets.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import firebase from '../connectors/firebase';
 
 export default class Tweets extends React.Component {
-  state = {};
+  state = {
+    users: {},
+    tweets: {}
+  };
+
+  componentWillMount() {
+    firebase
+      .database()
+      .ref('users')
+      .on('value', snapshot => {
+        const users = snapshot.val();
+        this.setState({ users });
+      });
+
+    firebase
+      .database()
+      .ref('tweets')
+      .on('value', snapshot => {
+        const tweets = snapshot.val();
+        this.setState({ tweets });
+      });
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Tweets!</Text>
+        <Text>{JSON.stringify(this.state, 0, 2)}</Text>
       </View>
     );
   }
